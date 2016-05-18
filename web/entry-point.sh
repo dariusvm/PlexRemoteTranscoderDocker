55fa53730b5cbb6cd76782923111044f283bf734
segator <isaac.aymerich@gmail.com>
2016-05-18 21:38:26 +0200

    Update entry-point.sh

--- web/entry-point.sh
+++ web/entry-point.sh
@@ -3,7 +3,7 @@
 
 printf "PUBLIC_DOMAIN=$PUBLIC_DOMAIN
 PLEX_PUBLIC_PORT=$PLEX_PUBLIC_PORT
-PRIVATE_WEB_DOMAIN=$(ifconfig eth0 | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f 1)" > /config/transcoderProvider.properties
+PRIVATE_WEB_DOMAIN=$(ifconfig eth0 | grep 'inet addr:' | cut -d : -f 2 | cut -d ' ' -f 1)" > /config/transcoderProvider.properties
 
 set -e
 exec runsvdir /etc/sv
